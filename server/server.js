76fe7e5fed689e5f62e3fa6473aad6f53743cffc
George Escobar <geo.escobar214@gmail.com>
2022-12-29 14:15:31 -0600

    auth refresh completed - ready for search bar and added UI

--- server/server.js
+++ server/server.js
@@ -29,4 +29,27 @@ app.post("/login", (req, res) => {
     });
 });
 
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
 app.listen(3001);
